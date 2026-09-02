0d6ea5ff572d02b5f1785934d2ba5f923c9e1338
mops1k <bednyj.mops@gmail.com>
2026-09-02 13:51:31 +0300

    Add missing System.IO using for File.Exists in App startup

--- VoiceTyper.App/App.xaml.cs
+++ VoiceTyper.App/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Globalization;
+using System.IO;
 using System.Windows;
 using Application = System.Windows.Application;
 using MessageBox = System.Windows.MessageBox;
